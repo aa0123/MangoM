aa2286bfb4768ca4a77d53c23c8a780b40a7fc6f
A Ali <alifarah0123@gmail.com>
2018-09-01 09:41:28 +0100

    textfield color change

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchfield, searchManga}) => {
 	return (
 		<div className='pa2'>
 			<input
-			className='pa3 ba b--green bg-lightest-blue' 
+			className='pa3 ba b--red bg-lightest-blue' 
 			type='search' 
 			placeholder='search manga here!'
 			onChange={searchManga} 
